5cbc24676104c92983c0e67cec733651d7f86ed6
petuniahtf <priscilliaweber6@hotmail.com>
2022-02-12 01:55:21 +0100

    Creat environnement

---
+++ src/Component/Footer/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './Footer.css'
+
+const Footer = () =>{
+
+    return(<footer>
+        <ul>
+            <li>Test1</li>
+            <li>Test2</li>
+            <li>Test3</li>
+            <li>Test4</li>
+        </ul>
+    </footer>
+    )
+} 
+
+export default Footer;
---
+++ src/Component/Footer/LittleFooter.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './LittleFooter.css'
+
+const LittleFooter = () =>{
+
+    return(<>
+    </>)
+} 
+
+export default LittleFooter;
