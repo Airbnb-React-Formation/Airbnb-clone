cf5f046fd62fbefec6cfd1014694837551d22f12
qcalmel <contact@quentincalmel.fr>
2022-03-06 17:46:12 +0100

    feat(results page): add popup on map price marker

--- src/components/Results/PriceMapMarker.js
+++ src/components/Results/PriceMapMarker.js
@@ -1,26 +1,65 @@
 import "./PriceMapMarker.css"
-import {Marker} from 'react-leaflet';
+import {Marker, Popup} from 'react-leaflet';
 import L from "leaflet";
 import * as ReactDOMServer from "react-dom/server";
+import ResultListCarousel from "./ResultListCarousel";
+import {RatingStar} from "../Icon/Icon";
 
-const PriceIcon = ({accommodation}) => {
+const PriceIcon = ({accommodation, selected}) => {
     return (
-        <div className="price-map-marker">
-            {accommodation.pricePerNight}€
+        <div className="price-map__wrapper">
+            <div className="price-map">
+                <div className={selected ? "price-map__marker selected" : "price-map__marker"}>
+                    <span className="bold">{accommodation.pricePerNight}€</span>
+                </div>
+            </div>
         </div>
     )
 }
 
-const PriceMapMarker = ({accommodation}) => {
+const PriceMapMarker = ({accommodation, onSelect, selectedMarker, onClick}) => {
     const getPriceIcon = (accommodation) => (
         L.divIcon({
-            html: ReactDOMServer.renderToString(<PriceIcon accommodation={accommodation}/>),
+            html: ReactDOMServer.renderToString(<PriceIcon accommodation={accommodation}
+                                                           selected={selectedMarker === accommodation.id}
+                                                           onClick={onClick}/>),
             className: "custom-marker",
         })
     )
 
     return (
-            <Marker position={accommodation.coordinates} icon={getPriceIcon(accommodation)}/>
+        <Marker
+            // pane="popupPane"
+            eventHandlers={{
+                click: () => {
+                    onSelect(accommodation.id)
+                }
+            }}
+            position={accommodation.coordinates}
+            icon={getPriceIcon(accommodation)}
+        >
+            <Popup>
+                <div
+                    className="price-map__popup">
+                    <ResultListCarousel pictureList={accommodation.pictures}/>
+                    <div className="price-map__popup-text">
+                        <div className="price-map__popup-comments">
+                            <span className="result-list-item__rating-icon"><RatingStar/></span>
+                            <span>4,5</span>
+                            <span className="result-list-item__comments"> (8)</span>
+                        </div>
+                        <div className="price-map__popup-housing-type">
+                            {accommodation.housingType + " : " + accommodation.placeType}
+                        </div>
+                        <div className="price-map__popup-title">{accommodation.title}</div>
+                        <div className="price-map__popup-price">
+                            <span className="bold">{accommodation.pricePerNight}€</span>
+                            <span>&nbsp;/ nuit</span>
+                        </div>
+                    </div>
+                </div>
+            </Popup>
+        </Marker>
     )
 }
 
--- src/components/Results/ResultMap.js
+++ src/components/Results/ResultMap.js
@@ -1,5 +1,5 @@
-import {useEffect} from "react";
-import {MapContainer, TileLayer, useMapEvent} from 'react-leaflet'
+import {useEffect, useState} from "react";
+import {MapContainer, TileLayer, useMapEvent, Pane} from 'react-leaflet'
 import PriceMapMarker from "./PriceMapMarker";
 import "leaflet/dist/leaflet.css"
 import "./ResultMap.css"
@@ -12,20 +12,33 @@ const MapEvents = ({handleBounds}) => {
     }, [])
 
     const map = useMapEvent({
-        moveend: () => handleBounds({
-            southWest: map.getBounds().getSouthWest(),
-            northEast: map.getBounds().getNorthEast()
-        }),
-        zoomend: () => handleBounds({
-            southWest: map.getBounds().getSouthWest(),
-            northEast: map.getBounds().getNorthEast()
-        })
+        moveend: () => {
+            handleBounds({
+                southWest: map.getBounds().getSouthWest(),
+                northEast: map.getBounds().getNorthEast(),
+            })
+            map.closePopup()
+        },
+        zoomend: () => {
+            handleBounds({
+                southWest: map.getBounds().getSouthWest(),
+                northEast: map.getBounds().getNorthEast()
+            })
+            map.closePopup()
+        }
     })
     return null
 }
 
+
 const ResultMap = ({coordinates, handleBounds, results}) => {
     coordinates = coordinates.split(',').reverse()
+    const [selectedMarker, setSelectedMarker] = useState('')
+
+    const handleSelect = (id) => {
+        setSelectedMarker(id)
+    }
+
     return (
 
         <div className="result-map">
@@ -34,14 +47,21 @@ const ResultMap = ({coordinates, handleBounds, results}) => {
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
+
                 {
                     Object.keys(results[0].coordinates).length !== 0 &&
                     (
                         results.map((accommodation, i) => (
-                            <PriceMapMarker key={i} accommodation={accommodation}/>
+                            <PriceMapMarker
+                                key={i}
+                                accommodation={accommodation}
+                                selectedMarker={selectedMarker}
+                                onSelect={handleSelect}
+                            />
                         ))
                     )
                 }
+                <Pane name="custom-page" style={{zIndex: 700}}/>
                 <MapEvents handleBounds={handleBounds}/>
             </MapContainer>
         </div>
