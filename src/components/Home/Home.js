6657989c3f693556e735523236fd5661499a9692
qcalmel <contact@quentincalmel.fr>
2022-03-08 11:27:55 +0100

    fix(home-page): remove header component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,23 +3,13 @@ import HomeHeaderCard from './HomeHeaderCard';
 import HomeHosting from './HomeHosting';
 import HomeExperiences from './HomeExperiences';
 import HomeGift from './HomeGift';
-import SearchBar from '../SearchBar/SearchBar';
 import Footer from '../Footer/Footer';
-import AccountMenuHeader from "../menu_account/AccountMenuHearder";
-import Header from "../header/Header";
 
 const Home = () => {
   return (
     <>
-      <header className='h__header'>
-          <div className="top-header">
-                <Header/>
-          </div>
-        <SearchBar />
-
-        <HomeHeaderCard />
-      </header>
       <main className='h__main'>
+          <HomeHeaderCard />
         <HomeExperiences />
         <HomeGift />
         <HomeHosting />
