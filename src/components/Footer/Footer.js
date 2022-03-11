47b7ffcab72414595bf154df149c583a979ece89
qcalmel <contact@quentincalmel.fr>
2022-03-11 10:09:43 +0100

    fix(footer): add vertical padding to link list footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,23 +2,24 @@ import React from "react";
 import './Footer.css'
 import LinkList from "./LinkList";
 import LittleFooter from "./LittleFooter"
-import { asistance, communauté, acceuilEtVoyageur,aPropos } from "./datalinklist";
+import {asistance, communauté, acceuilEtVoyageur, aPropos} from "./datalinklist";
 
 
-const Footer = () =>{
+const Footer = () => {
 
-    return(<footer className='thefooter'>
-        <section className="resection">
-        <LinkList  title = "Asistance" tableau={asistance}/>
-        <LinkList  title = "Communauté" tableau={communauté}/>
-        <LinkList  title = "Accueil et voyageur" tableau={acceuilEtVoyageur}/>
-        <LinkList  title = "A Propos" tableau={aPropos}/>
-        </section>
-        <section>
-        <LittleFooter></LittleFooter>
-        </section>
+    return (
+        <footer className='thefooter'>
+            <section className="resection">
+                <LinkList title="Asistance" tableau={asistance}/>
+                <LinkList title="Communauté" tableau={communauté}/>
+                <LinkList title="Accueil et voyageur" tableau={acceuilEtVoyageur}/>
+                <LinkList title="A Propos" tableau={aPropos}/>
+            </section>
+            <section>
+                <LittleFooter/>
+            </section>
         </footer>
     )
-} 
+}
 
 export default Footer;
