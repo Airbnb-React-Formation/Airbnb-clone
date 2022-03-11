91513a9f98b61129d33114b756f2e044e287f759
qcalmel <contact@quentincalmel.fr>
2022-03-11 11:21:57 +0100

    fix(footer):remove fixed sized icon

--- src/components/Footer/SocialComponent.js
+++ src/components/Footer/SocialComponent.js
@@ -1,14 +1,21 @@
 import React from "react";
 import './SocialComponent.css'
-import { IconFacebook, IconTwitter, IconInstagram } from "../Icon/Icon";
+import {IconFacebook, IconTwitter, IconInstagram, Icon} from "../Icon/Icon";
 
-const SocialComponents = () =>{
+const SocialComponents = () => {
 
-    return(<div className="socialComponent">
-        <IconFacebook/>
-        <IconInstagram/>
-        <IconTwitter/>
-    </div>)
-} 
+    return (
+        <div className="socialComponent">
+            <Icon className="social-component__icon">
+                <IconFacebook/>
+            </Icon>
+            <Icon className="social-component__icon">
+                <IconInstagram/>
+            </Icon>
+            <Icon className="social-component__icon">
+                <IconTwitter/>
+            </Icon>
+        </div>)
+}
 
 export default SocialComponents;
