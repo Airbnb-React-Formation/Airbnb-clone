230eea8058f3e832ed1f3313aace3504f4134e19
qcalmel <contact@quentincalmel.fr>
2022-03-11 16:36:17 +0100

    fix(home-page): fix padding and background color

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,21 +5,22 @@ import HomeExperiences from './HomeExperiences';
 import HomeGift from './HomeGift';
 
 const Home = () => {
-  return (
-    <>
-      <main className='h__main'>
-          <HomeHeaderCard />
-        <HomeExperiences />
-        <HomeGift />
-        <HomeHosting />
-        <div style={{
-          width: '100%',
-          minHeight: '5rem',
-          textAlign: 'center'
-        }}>composant temporaire inspiration</div>
-      </main>
-    </>
-  );
+    return (
+        <>
+            <main className='h__main'>
+                <HomeHeaderCard/>
+                <HomeExperiences/>
+                <HomeGift/>
+                <HomeHosting/>
+                <div style={{
+                    width: '100%',
+                    minHeight: '5rem',
+                    textAlign: 'center'
+                }}>composant temporaire inspiration
+                </div>
+            </main>
+        </>
+    );
 }
 
 export default Home;
