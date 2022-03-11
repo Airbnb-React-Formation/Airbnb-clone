b2fac6494e3a98c4eaf31c642e54ad74f5dcf8eb
petuniahtf <priscilliaweber6@hotmail.com>
2022-03-11 19:00:26 +0100

    hook

--- src/components/Detail/DateModal.js
+++ src/components/Detail/DateModal.js
@@ -1,14 +1,16 @@
 import React, { useRef } from "react"
 import CalendarPanel from "../SearchBar/CalendarPanel.js";
 import { FieldPanel }  from "../SearchBar/SearchBarField.js";
-//import
+import useClickOutside from '../../hook/useClickOutside'
 
 
 const DateModal = ({calendarProps :{startDate, endDate, setEndDate, setStartDate, selectedField, setSelectedField}}) =>{
-     //const dateModalRef = useRef() 
-     //useClickOutside() ref={dateModalRef}
+     const dateModalRef = useRef() 
+     useClickOutside(dateModalRef, ()=>{setSelectedField('')})
     return (
-    <div  style={{position :'relative'}}>  
+    <div  
+    ref={dateModalRef} 
+    style={{position :'relative'}}>  
         
           <input type="text" name="start date" value={startDate?.format('DD/MM/YY')} onClick={()=>{setSelectedField('startDate')}}/> 
           <input type="text" name="end date" value={endDate?.format('DD/MM/YY')} onClick={()=>{setSelectedField('endDate')}}/> 
