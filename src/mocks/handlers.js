997b2f8bf5cb28e7c4efdbdf0548478d06407369
qcalmel <contact@quentincalmel.fr>
2022-03-13 22:51:35 +0100

    feat(msw): add return of 404 status when id is not found

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -10,7 +10,12 @@ export const handlers = [
     }),
     rest.get('/api/accommodations/:id', (req,res,ctx)=>{
         const {id} = req.params
-        const index = accommodations.findIndex(accommodation => accommodation.id === id)
+        const index = accommodations.findIndex(accommodation => accommodation.id === +id)
+        if (index < 0) {
+            return res(
+                ctx.status(404)
+            )
+        }
         return res(
             ctx.status(200),
             ctx.json(accommodations[index])
