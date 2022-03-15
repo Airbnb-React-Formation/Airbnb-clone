86b51e74c9dafcf49db49e288f6ab8aad826defc
qcalmel <contact@quentincalmel.fr>
2022-03-15 22:41:31 +0100

    feat(sign-in-modal): handle validator on password field

--- src/components/LoginModal/LoginModalPassword.js
+++ src/components/LoginModal/LoginModalPassword.js
@@ -14,9 +14,11 @@ const LoginModalPassword = ({formValue, handleChange, handleSubmit, isSubmit}) =
                 placeholder="Mot de passe"
                 value={formValue.password.value}
                 onChange={handleChange}
+                error={formValue.password.error}
+                helperText={isSubmit && formValue.password.error ? 'Le mot de passe doit contenir au moins une minuscule, une majuscule, un chiffre et doit avoir une taille comprise entre 6 et 13 caractères.':''}
             />
             <Box mt="16px" mb="24px">
-                <Button size="large" variant="contained-red" fullWidth={true}>Se connecter</Button>
+                <Button size="large" variant="contained-red" fullWidth={true} onClick={handleSubmit}>Se connecter</Button>
             </Box>
             <Typography>
                 <span className="sign-in-modal__forgot-password">Mot de passe oublié ?</span>
