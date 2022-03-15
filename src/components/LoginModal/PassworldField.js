3c9fb6b8d8c6164a2de96c2f8c7b13d0ef461a4d
qcalmel <contact@quentincalmel.fr>
2022-03-15 18:00:42 +0100

    feat(sign-in-modal): create PasswordField component

---
+++ src/components/LoginModal/PassworldField.js
@@ -0,0 +1,31 @@
+import {InputAdornment, TextField} from "@mui/material";
+import {useState} from "react";
+
+const PasswordField = ({id,name,label,fullWidth,placeholder,value,onChange,error,helperText}) => {
+    const [showPassword,setShowPassword] = useState(false)
+    return (
+        <TextField type={showPassword ? "text":"password"}
+                   id={id}
+                   name={name}
+                   label={label}
+                   fullWidth={fullWidth}
+                   placeholder={placeholder}
+                   value={value}
+                   onChange={onChange}
+                   error={error}
+                   helperText={helperText}
+                   InputProps={{
+                       endAdornment:(
+                           <InputAdornment
+                               style={{cursor:"pointer"}}
+                               id="show-password"
+                               position="end"
+                               onClick={()=>setShowPassword(!showPassword)}
+                           >
+                               {showPassword ? "Masquer":"Afficher"}
+                           </InputAdornment>)
+                   }}/>
+    )
+}
+
+export default PasswordField
