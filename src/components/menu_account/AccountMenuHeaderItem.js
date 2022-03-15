4969bc5316c774da278ed5bb2b28a0e314f92eef
qcalmel <contact@quentincalmel.fr>
2022-03-15 18:02:45 +0100

    feat(header): add sign-in modal in header

--- src/components/menu_account/AccountMenuHeaderItem.js
+++ src/components/menu_account/AccountMenuHeaderItem.js
@@ -6,13 +6,13 @@ function AccountMenuHeaderNotification() {
     )
 }
 
-function AccountMenuHeaderItem({text, bold, notification}) {
+function AccountMenuHeaderItem({text, bold, notification,onClick}) {
     if (text == null) {
         return <div className="section-bar"/>
     }
     const className = (bold) ? "bold" : ""
     return (
-        <div className="header-profile-menu__item">
+        <div className="header-profile-menu__item" onClick={onClick}>
             <span className={className}>{text}
                 {notification ? <AccountMenuHeaderNotification/> : <span/>}
             </span>
--- src/components/menu_account/AccountMenuHearder.js
+++ src/components/menu_account/AccountMenuHearder.js
@@ -1,63 +1,89 @@
 import "./AccountMenuHeader.scss";
-import React, {useRef} from "react";
+import React, {useRef, useState} from "react";
 import AccountMenuHeaderItem from "./AccountMenuHeaderItem";
 import {MenuBarIcon, MenuProfileIcon} from "../Icon/Icon";
 import useClickOutside from "../../hook/useClickOutside";
+import LoginModal from "../LoginModal/LoginModal";
+import {DialogContent, Modal} from "@mui/material";
 
-const payload_disconnect = [
-    {text: "Inscription", bold : true},
-    { text: "Connexion" , bold: false },
-    {} ,
-    {text: "Hebergement de voyageur", bold: false } ,
-    {text: "Créer une Expérience ", bold: false},
-    {},
-    {text: "Aide", bold: false }
-]
 
-const payload_connect = [
-    {text: "Messages" , bold : true},
-    {text: "Notification", bold: true, notification: true},
-    {text: "Voyages" , bold: true},
-    {text:  "Favoris", bold: true},
-    {},
-    {text: "Gérer mes annonces", bold: false, notification: true},
-    {text: "Créer une Expérience ", bold: false},
-    {text: "Parrainer un hôte ", bold: false},
-    {text: "Compte", bold: false},
-    {},
-    {text: "Aide", bold: false }
-]
-
-function AccountMenuHeader(){
-    const [displayMenuHeader , setDisplayMenuHeader ] = React.useState(false)
-    const handleClick = () =>{
+function AccountMenuHeader() {
+    const [displayMenuHeader, setDisplayMenuHeader] = React.useState(false)
+    const [isSignInLogInModal, setIsSignInLogInModal] = useState(false)
+    const handleOpen = () => {
+        setDisplayMenuHeader(false)
+        setIsSignInLogInModal(true)
+    }
+    const handleClose = () => setIsSignInLogInModal(false)
+    const handleClick = () => {
         setDisplayMenuHeader(!displayMenuHeader);
     }
     const accountMenuRef = useRef()
-    useClickOutside(accountMenuRef,()=>setDisplayMenuHeader(false))
+    useClickOutside(accountMenuRef, () => setDisplayMenuHeader(false))
     return (
-        <div ref={accountMenuRef} className="header-profile-menu-parent-button">
-            <button onClick={handleClick} className="header-profile-menu-button">
-                <div>
-                    <MenuBarIcon/>
-                </div>
-                <div>
-                    <MenuProfileIcon size={"16px"}/>
-                </div>
-            </button>
-            {displayMenuHeader ? <AccountMenuHeaderOnClick /> : <span/>}
-        </div>
+        <>
+            <div ref={accountMenuRef} className="header-profile-menu-parent-button">
+                <button onClick={handleClick} className="header-profile-menu-button">
+                    <div>
+                        <MenuBarIcon/>
+                    </div>
+                    <div>
+                        <MenuProfileIcon size={"16px"}/>
+                    </div>
+                </button>
+                {displayMenuHeader ? <AccountMenuHeaderOnClick onClickActions={{signInLogInModal: handleOpen}}/> :
+                    <span/>}
+            </div>
+            <Modal open={isSignInLogInModal} onClose={handleClose}>
+                <DialogContent>
+                    <LoginModal onClose={handleClose}/>
+                </DialogContent>
+            </Modal>
+        </>
     )
 
 }
 
-function AccountMenuHeaderOnClick(){
- //   const [connected, setConnected ]= React.useState();
+function AccountMenuHeaderOnClick({onClickActions}) {
 
-    const connected = true;
+    const payload_disconnect = [
+        {text: "Inscription", bold: true, onClick: onClickActions.signInLogInModal},
+        {text: "Connexion", bold: false, onClick: onClickActions.signInLogInModal},
+        {},
+        {text: "Hebergement de voyageur", bold: false},
+        {text: "Créer une Expérience ", bold: false},
+        {},
+        {text: "Aide", bold: false}
+    ]
+    const payload_connect = [
+        {text: "Messages", bold: true},
+        {text: "Notification", bold: true, notification: true},
+        {text: "Voyages", bold: true},
+        {text: "Favoris", bold: true},
+        {},
+        {text: "Gérer mes annonces", bold: false, notification: true},
+        {text: "Créer une Expérience ", bold: false},
+        {text: "Parrainer un hôte ", bold: false},
+        {text: "Compte", bold: false},
+        {},
+        {text: "Aide", bold: false}
+    ]
+    //   const [connected, setConnected ]= React.useState();
+
+    const connected = false;
     const payload = (connected) ? payload_connect : payload_disconnect;
-    const listMenu = payload.map((element,i) =>{ return  <AccountMenuHeaderItem key={i} text={element.text} bold={element.bold} notification={element.notification}  />})
-    return  <div className="header-profile-menu">
+    const listMenu = payload.map((element, i) => {
+        return (
+            <AccountMenuHeaderItem
+                key={i}
+                text={element.text}
+                bold={element.bold}
+                notification={element.notification}
+                onClick={element.onClick}
+            />
+        )
+    })
+    return <div className="header-profile-menu">
         {listMenu}
     </div>
 }
