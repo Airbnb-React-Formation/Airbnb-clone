7bf58daf90c491ea80ebf47950916642fffd7cb5
qcalmel <contact@quentincalmel.fr>
2022-03-15 17:57:49 +0100

    feat(theme): add context to use MUI custom theme

--- src/App.js
+++ src/App.js
@@ -7,22 +7,28 @@ import BecomeHostIntro from "./components/Hosting/BecomeHostIntro";
 import Header from "./components/header/Header";
 import "./App.css"
 import Footer from "./components/Footer/Footer";
+import { ThemeProvider} from "@mui/material";
+import MuiTheme from "./theme/MuiTheme";
+
+
 
 function App() {
     return (
         <div className="app">
-            <BrowserRouter>
-                <Header/>
-                <Routes>
-                    <Route path="/" element={<Home/>}/>
-                    <Route path="/hosting" element={<Hosting/>}/>
-                    <Route path="/becomeHost" element={<BecomeHostIntro/>}/>
-                    <Route path="/search" element={<Results/>}/>
-                    <Route path="/detail" element={<Detail/>}/>
-                    <Route path="*" element={<Navigate to="/"/>}/>
-                </Routes>
-                <Footer/>
-            </BrowserRouter>
+            <ThemeProvider theme={MuiTheme}>
+                <BrowserRouter>
+                    <Header/>
+                    <Routes>
+                        <Route path="/" element={<Home/>}/>
+                        <Route path="/hosting" element={<Hosting/>}/>
+                        <Route path="/becomeHost" element={<BecomeHostIntro/>}/>
+                        <Route path="/search" element={<Results/>}/>
+                        <Route path="/detail" element={<Detail/>}/>
+                        <Route path="*" element={<Navigate to="/"/>}/>
+                    </Routes>
+                    <Footer/>
+                </BrowserRouter>
+            </ThemeProvider>
         </div>
     );
 }
