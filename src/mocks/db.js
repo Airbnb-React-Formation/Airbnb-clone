89cc3cf9adb0b6fb2d31feea6556aedb34c8f34f
qcalmel <contact@quentincalmel.fr>
2022-04-08 10:26:16 +0200

    feat(api): add functions to handle search history in search bar destination panel

--- src/mocks/db.js
+++ src/mocks/db.js
@@ -2,6 +2,7 @@ import CryptoJS from "crypto-js";
 const Buffer = require('buffer/').Buffer
 
 const localStorageKey = 'db-back-airbnb-clone-users'
+const localStorageSearchHistoryKey = 'db-back-airbnb-clone-search-history'
 
 const loadUsers = () => {
     const users = JSON.parse(window.localStorage.getItem(localStorageKey))
@@ -23,6 +24,27 @@ const saveUsers = (users) => {
     window.localStorage.setItem(localStorageKey, JSON.stringify(users))
 }
 
+const loadSearchHistory = () => {
+    const searchHistory = JSON.parse(window.localStorage.getItem(localStorageSearchHistoryKey))
+    return searchHistory ?? {}
+}
+
+const loadSearchHistoryById = (id) => {
+    const userSearchHistory = loadSearchHistory()[id] ?? []
+    return userSearchHistory
+}
+
+const saveSearchHistory = (id, url) => {
+    const searchHistory = loadSearchHistory()
+    let userSearchHistory = loadSearchHistoryById(id)
+    if (userSearchHistory.length >= 5) {
+        userSearchHistory = userSearchHistory.slice(-4)
+    }
+    userSearchHistory.push(url)
+    searchHistory[id] = userSearchHistory
+    window.localStorage.setItem(localStorageSearchHistoryKey, JSON.stringify(searchHistory))
+}
+
 const createUser = async ({username, password}) => {
     validateUser({username, password})
     const id = hashcode(username)
@@ -40,7 +62,7 @@ const createUser = async ({username, password}) => {
     return await loadUserById(id)
 }
 
-const clean =(user) => {
+const clean = (user) => {
     if (user.passwordHash) {
         const {passwordHash, ...rest} = user
         return rest
@@ -73,11 +95,11 @@ const authenticate = async ({username, password}) => {
     const id = hashcode(username)
     const user = (await loadUserById(id) || {})
     if (user.passwordHash === hashcode(password)) {
-        return {...clean(user),token : Buffer.from(user.id).toString('base64')}
+        return {...clean(user), token: Buffer.from(user.id).toString('base64')}
     }
     const error = new Error("Nom d'utilisateur ou mot de passe incorrect")
     error.status = 400
     throw error
 }
 
-export {authenticate,loadUserById,loadUsers,isUserExists,createUser}
+export {authenticate, loadUserById, loadUsers, isUserExists, createUser, saveSearchHistory, loadSearchHistoryById}
