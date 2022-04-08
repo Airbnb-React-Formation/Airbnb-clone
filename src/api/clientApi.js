863ef524e545cd3c11d4ae63b551b5277b75de18
qcalmel <contact@quentincalmel.fr>
2022-04-08 10:28:00 +0200

    feat(app): add function to use api for saving and getting search history

--- src/api/clientApi.js
+++ src/api/clientApi.js
@@ -1,3 +1,5 @@
+import {getToken} from "./authApi";
+import searchParamsToObject from "../helper/searchParamsToObject";
 
 
 const clientApi = async endpoint => {
@@ -10,7 +12,7 @@ const clientApi = async endpoint => {
                     reject('Erreur lors de la récupération des données')
                 }
             })
-        .catch(err=>reject("erreur de l'opération fetch :" +err.message))
+            .catch(err => reject("erreur de l'opération fetch :" + err.message))
     })
 }
 
@@ -18,15 +20,43 @@ const clientAuth = (endpoint, {token, data} = {}) => {
     let config = {
         headers: {
             Authorization: token ? `Bearer ${token}` : undefined,
-            'Content-Type': data ? 'application/json' : undefined,
+            'Content-Type': 'application/json',
         },
-        method : data ? 'POST' : 'GET',
+        method: data ? 'POST' : 'GET',
     }
-    if(data) config = {...config,body:JSON.stringify(data)}
-    return fetch(`/auth/${endpoint}`,config)
+    if (data) config = {...config, body: JSON.stringify(data)}
+    return fetch(`/auth/${endpoint}`, config)
+}
+
+const saveSearchHistory = async (search) => {
+    const token = await getToken()
+    const data = {search}
+    clientAuth('search-history', {token, data})
+        .catch(err => console.log(err))
+}
+
+const getSearchHistory = async () => {
+    const token = await getToken()
+    return clientAuth('search-history', {token}).then(res => res.json())
+        .then(data => {
+            return data?.map(search => {
+                const query = searchParamsToObject(search)
+                return {
+                    url: search,
+                    destination: query.destination,
+                    startDate: query.startdate,
+                    endDate: query.enddate
+                }
+            })
+
+        })
+        .catch(err => console.log(err))
+
 }
 
 export {
     clientApi,
-    clientAuth
+    clientAuth,
+    saveSearchHistory,
+    getSearchHistory,
 }
