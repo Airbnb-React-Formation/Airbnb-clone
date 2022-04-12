6c53a6feccb1603b1f81afa6c0208dfcf43c96a5
qcalmel <contact@quentincalmel.fr>
2022-04-12 09:23:54 +0200

    refactor(room-page): change class name to match with BEM React convention naming

--- src/components/Room/RoomHeader.js
+++ src/components/Room/RoomHeader.js
@@ -3,33 +3,33 @@ import "./RoomHeader.css"
 
 const RoomHeader = ({title,location}) => {
     return (
-        <div className="room-page__header">
-            <div className="room-page__header-title bold">
+        <div className="RoomHeader">
+            <div className="RoomHeader__title bold">
                 {title?.toUpperCase()}
             </div>
-            <div className="room-page__header-subtitle-container">
-                <div className="room-page__header-subtitle-left">
+            <div className="RoomHeader__subtitle-container">
+                <div className="RoomHeader__subtitle-left">
                     <div className="result-list-item__rating">
                         <span className="result-list-item__rating-icon"><RatingStar/></span>
                         <span>4,5</span>
                         <span className="result-list-item__comments"> (8 commentaires)</span>
                     </div>
-                    <div className="room-page__header-subtitle-separator">
+                    <div className="RoomHeader__separator">
                         <span>·</span>
                     </div>
-                    <div className="room-page__header-subtitle-location">
+                    <div className="RoomHeader__location">
                         {location}
                     </div>
                 </div>
-                <div className="room-page__header-subtitle-right">
-                    <button className="room-page__header-subtitle-button">
-                    <Icon className="room-page__header-subtitle-button-icon">
+                <div className="RoomHeader__subtitle-right">
+                    <button className="RoomHeader__button">
+                    <Icon className="RoomHeader__icon">
                         <ShareIcon/>
                     </Icon>
                         <span>Partager</span>
                     </button>
-                    <button className="room-page__header-subtitle-button">
-                        <Icon className="room-page__header-subtitle-button-icon">
+                    <button className="RoomHeader__button">
+                        <Icon className="RoomHeader__icon">
                             <HeartIcon/>
                         </Icon>
                         <span>Enregistrer</span>
