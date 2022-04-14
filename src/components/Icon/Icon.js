c0750761f778932dd60bce1c80d4cd9aeb8c878b
qcalmel <contact@quentincalmel.fr>
2022-04-14 21:16:41 +0200

    feat(app): add BedDouble icon

--- src/components/Icon/Icon.js
+++ src/components/Icon/Icon.js
@@ -251,3 +251,9 @@ export const ChevronRightIcon = () => (
         <path d="m4.29 1.71a1 1 0 1 1 1.42-1.41l8 8a1 1 0 0 1 0 1.41l-8 8a1 1 0 1 1 -1.42-1.41l7.29-7.29z" fillRule="evenodd"/>
     </svg>
 )
+
+export  const BedDoubleIcon = () => (
+    <svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="presentation" focusable="false">
+        <path d="M26 4a2 2 0 0 1 1.995 1.85L28 6v7.839l1.846 5.537a3 3 0 0 1 .115.468l.03.24.009.24V30h-2v-2H4v2H2v-9.675a3 3 0 0 1 .087-.717l.067-.232L4 13.836V6a2 2 0 0 1 1.697-1.977l.154-.018L6 4zm2 18H4v4h24zm-1.388-6H5.387l-1.333 4h23.891zM26 6H6v8h2v-4a2 2 0 0 1 1.85-1.995L10 8h12a2 2 0 0 1 1.995 1.85L24 10v4h2zm-11 4h-5v4h5zm7 0h-5v4h5z"/>
+    </svg>
+)
