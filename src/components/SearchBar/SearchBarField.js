bf358e8f3867af9fecbbb140b59133c675860e3d
qcalmel <contact@quentincalmel.fr>
2022-04-16 21:18:46 +0200

    feat(search-bar): add prop to FieldPanel for overriding css class

--- src/components/SearchBar/SearchBarField.js
+++ src/components/SearchBar/SearchBarField.js
@@ -62,7 +62,7 @@ const SearchBarField = ({
     )
 }
 
-const FieldPanel = ({children, align, isSelected, fieldName}) => {
+const FieldPanel = ({children, align, isSelected, fieldName,className}) => {
     if (!isSelected) return null
     return (
         <>
@@ -71,6 +71,7 @@ const FieldPanel = ({children, align, isSelected, fieldName}) => {
                     (align === "right" ? "search-bar-panel align-panel-right" : "search-bar-panel")
                     + (fieldName === 'startDate' ? " search-bar-panel-start-date" : "")
                     + (fieldName === 'endDate' ? " search-bar-panel-end-date" : "")
+                    + (className ? " " + className : "")
                 }
             >
                 {children}
