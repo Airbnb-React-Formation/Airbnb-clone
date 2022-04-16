7649e6f92c3a926fedf4c3cd2d6531275a3a204f
qcalmel <contact@quentincalmel.fr>
2022-04-16 15:56:47 +0200

    feat(app): add function for capitalize first letter of a string

--- src/helper/helper.js
+++ src/helper/helper.js
@@ -1,3 +1,7 @@
 const isMany = (number) => number > 1 ? "s" : ""
 
-export {isMany}
+const capitalizeFirstLetter = (string) => (
+    string.charAt(0).toUpperCase() + string.slice(1)
+)
+
+export {isMany,capitalizeFirstLetter}
