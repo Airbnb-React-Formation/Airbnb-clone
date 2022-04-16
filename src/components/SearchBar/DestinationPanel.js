bf1d6f87e11bf246aeaff185b7cbe75c15340e29
qcalmel <contact@quentincalmel.fr>
2022-04-16 22:16:19 +0200

    fix(search-bar): fix empty field panel showing before content was loaded

--- src/components/SearchBar/DestinationPanel.js
+++ src/components/SearchBar/DestinationPanel.js
@@ -12,11 +12,14 @@ const DestinationPanel = ({search, onSelect}) => {
     const [searchHistory, setSearchHistory] = useState([])
     const navigate = useNavigate()
     const {authUser} = useAuth()
+    const [isLoaded, setIsLoaded] = useState(false)
     useEffect(() => {
         if (authUser) {
             const fetchSearchHistory = async () => {
-                const searches = await getSearchHistory()
-                setSearchHistory(searches)
+                getSearchHistory().then((searches) => {
+                    setSearchHistory(searches)
+                    setIsLoaded(true)
+                })
             }
             fetchSearchHistory().catch(err => console.log(err))
         }
@@ -30,58 +33,66 @@ const DestinationPanel = ({search, onSelect}) => {
                 return authorizedType.indexOf(place.properties.type) !== -1
             })
             setResult(filteredPlaces.slice(0, 5))
+
+            if ((!filteredPlaces.length && search) || (!search && !searchHistory.length)) setIsLoaded(false)
+            else setIsLoaded(true)
+            // else if(((!searchHistory.length || !isLoaded) && filteredPlaces.length)||(!search && searchHistory.length)) setIsLoaded(true)
         })
     }, [search])
-    return (<div className="suggestions-list">
-        {
-            result.length ?
-                result.map(({geometry, properties: {name, type, city, country, county}}, i) => (
-                    <div key={i} className="suggestion" onClick={() => {
-                        onSelect({name: name, coordinates: geometry.coordinates})
-                    }}>
-                        <div className="localisation-icon-container">
-                            <Icon className="destination-panel__icon"><LocationIcon/></Icon>
-                            </div>
-                        <div>
-                            {name}
-                            {(type === 'street' && city) && (", " + city)}
-                            {(type === 'locality' && county) && (", " + county)}
-                            {((type === 'city' || type === 'state' || type === 'county') && country) && (", " + country)}
-                        </div>
-                    </div>))
-                :
-                (
-                    searchHistory.length ?
-                        <>
-                            <div className="suggestion__title bold">Recherches récentes</div>
-                            {searchHistory.map(({
-                                                    url,
-                                                    destination,
-                                                    startDate,
-                                                    endDate,
-                                                    adults,
-                                                    children,
-                                                    infants,
-                                                    pets
-                                                }, i) => (
-                                <div key={i} className="suggestion" onClick={() => {
-                                    navigate(`/search/${url}`)
-                                }}>
-                                    <div className="localisation-icon-container">
-                                        <Icon className="destination-panel__icon"><HistoryIcon/></Icon>
-                                    </div>
-                                    <div>
-                                        <div>{destination}</div>
-                                        <div
-                                            className="suggestion__subtext">{`${moment(startDate).format("DD MMM")} - ${moment(endDate).format("DD MMM")} • ${(adults + children) + " voyageur" + isMany(adults + children)} `}</div>
-                                    </div>
+    return (
+        isLoaded ?
+            <div className="suggestions-list">
+                {
+                    result.length ?
+                        result.map(({geometry, properties: {name, type, city, country, county}}, i) => (
+                            <div key={i} className="suggestion" onClick={() => {
+                                onSelect({name: name, coordinates: geometry.coordinates})
+                            }}>
+                                <div className="localisation-icon-container">
+                                    <Icon className="destination-panel__icon"><LocationIcon/></Icon>
                                 </div>
-                            ))}
-                        </>
-                        : null
-                )
-        }
-    </div>)
+                                <div>
+                                    {name}
+                                    {(type === 'street' && city) && (", " + city)}
+                                    {(type === 'locality' && county) && (", " + county)}
+                                    {((type === 'city' || type === 'state' || type === 'county') && country) && (", " + country)}
+                                </div>
+                            </div>))
+                        :
+                        (
+                            searchHistory?.length ?
+                                <>
+                                    <div className="suggestion__title bold">Recherches récentes</div>
+                                    {searchHistory.map(({
+                                                            url,
+                                                            destination,
+                                                            startDate,
+                                                            endDate,
+                                                            adults,
+                                                            children,
+                                                            infants,
+                                                            pets
+                                                        }, i) => (
+                                        <div key={i} className="suggestion" onClick={() => {
+                                            navigate(`/search/${url}`)
+                                        }}>
+                                            <div className="localisation-icon-container">
+                                                <Icon className="destination-panel__icon"><HistoryIcon/></Icon>
+                                            </div>
+                                            <div>
+                                                <div>{destination}</div>
+                                                <div
+                                                    className="suggestion__subtext">{`${moment(startDate).format("DD MMM")} - ${moment(endDate).format("DD MMM")} • ${(adults + children) + " voyageur" + isMany(adults + children)} `}</div>
+                                            </div>
+                                        </div>
+                                    ))}
+                                </>
+                                : null
+                        )
+                }
+            </div>
+            :
+            null)
 }
 
 export default DestinationPanel
