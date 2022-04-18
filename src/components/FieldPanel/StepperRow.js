a08daf4fcaf599b85c4d3d4b14c36af034d0915e
qcalmel <contact@quentincalmel.fr>
2022-04-18 15:51:59 +0200

    refactor(field-panel): change class name of StepperRow to match with BEM React naming convention

--- src/components/FieldPanel/StepperRow.js
+++ src/components/FieldPanel/StepperRow.js
@@ -4,14 +4,14 @@ const StepperRow = ({children,maxValue,value,handleChange,minValue = 0}) => {
     const handleDecrease = () => handleChange(value -1)
 
     return (
-    <div className="stepper-row">
+    <div className="StepperRow">
             <div>
                 {children}
             </div>
-            <div className="stepper">
-                <button className="stepper-button" disabled={value === minValue } onClick={handleDecrease}>-</button>
+            <div className="Stepper">
+                <button className="Stepper__button" disabled={value === minValue } onClick={handleDecrease}>-</button>
                 <div className="value">{value}</div>
-                <button className="stepper-button" disabled={value === maxValue} onClick={handleIncrease}>+</button>
+                <button className="Stepper__button" disabled={value === maxValue} onClick={handleIncrease}>+</button>
             </div>
         </div>
     )
@@ -19,12 +19,12 @@ const StepperRow = ({children,maxValue,value,handleChange,minValue = 0}) => {
 }
 
 const StepperTitle = ({children}) => (
-    <div className="stepper-title">{children}</div>
+    <div className="Stepper__title">{children}</div>
     )
 
 
 const StepperSubTitle = ({children}) => (
-    <div className="stepper-subtitle">{children}</div>
+    <div className="Stepper__subtitle">{children}</div>
     )
 
 const StepperLink = ({children}) => (
